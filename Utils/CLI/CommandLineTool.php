aba9630783e2b3dd006f1ca45cb20f51a02980a6
riizlaah <100104632+riizlaah@users.noreply.github.com>
2025-09-28 14:23:53 +0000

    add command not found on cli tool

--- Utils/CLI/CommandLineTool.php
+++ Utils/CLI/CommandLineTool.php
@@ -24,6 +24,10 @@ class CommandLineTool {
     if(isset($args[1])) $commands[1] = $args[1];
     unset($args[0], $args[1], $raw);
     $full_cmd = implode('_', $commands);
+    if(method_exists(Commands::class, $full_cmd)) {
+      echo "command not found: \033[38;05;11m".str_replace('_', ' ', $full_cmd)."\033[0m";
+      return;
+    }
     $cmd_obj = new Commands($parsed[1]);// $parsed[1] adalah 'option'
     call_user_func_array([$cmd_obj, $full_cmd], $args);
   }
